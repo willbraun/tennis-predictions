14f10e4f16c77c421353df807034fc7700fbf3ed
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-09-21 13:57:35 -0400

    getting player id from name

--- index.py
+++ index.py
@@ -2,10 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-# Get prematch odds for all ATP matches on current day
 session = requests.Session()
+
+def call_url(url):
+    return session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+# Get prematch odds for all ATP matches on current day
+
 odds_url = 'https://www.bovada.lv/services/sports/event/coupon/events/A/description/tennis/atp?lang=en&eventsLimit=50&preMatchOnly=true&marketFilterId=def'
-odds_response = session.get(odds_url, headers={'User-Agent': 'Mozilla/5.0'})
+odds_response = call_url(odds_url)
 odds_json = json.loads(odds_response.text)
 data = []
 
@@ -24,13 +29,28 @@ for tournament in odds_json:
     events_detailed = list(map(get_outcomes, events))
     data = data + events_detailed
 
-print(data)
+# print(data)
+
+# Get UTS id based on the player name
+
+def get_id(name):
+    term = name.replace(' ', '+').lower()
+    search_url = f'https://www.ultimatetennisstatistics.com/autocompletePlayer?term={term}'
+    search_response = call_url(search_url)
+    search_json = json.loads(search_response.text)
+    player = list(filter(lambda x: (x['value'] == name), search_json))[0]
+    return player['id']
+
+test_name = data[0][0]['name']
+test = get_id(test_name)
+print(test_name)
+# print(test)
 
 # Get probability that player 1 will win
-url = 'https://www.ultimatetennisstatistics.com/h2hHypotheticalMatchup?playerId1=11127&playerId2=6269'
-response = session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+prob_url = 'https://www.ultimatetennisstatistics.com/h2hHypotheticalMatchup?playerId1=11127&playerId2=6269'
+prob_response = call_url(prob_url)
 
-doc = BeautifulSoup(response.text, 'html.parser')
+doc = BeautifulSoup(prob_response.text, 'html.parser')
 win_prob_row = doc.find(text='Win Probability')
 p1_win_prob = float(win_prob_row.parent.parent.find('h4').contents[0].replace('%', ''))
 
