88b07c26a095993e8a79edd2e87afd4a267993b6
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-09-21 13:33:29 -0400

    getting all matches and odds for each the current day

--- index.py
+++ index.py
@@ -2,19 +2,29 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-# Get odds for the first match on the list (to change)
+# Get prematch odds for all ATP matches on current day
 session = requests.Session()
-odds_url = "https://www.bovada.lv/services/sports/event/coupon/events/A/description/tennis?lang=en&eventsLimit=1"
+odds_url = 'https://www.bovada.lv/services/sports/event/coupon/events/A/description/tennis/atp?lang=en&eventsLimit=50&preMatchOnly=true&marketFilterId=def'
 odds_response = session.get(odds_url, headers={'User-Agent': 'Mozilla/5.0'})
 odds_json = json.loads(odds_response.text)
-outcomes = odds_json[0]['events'][0]['displayGroups'][0]['markets'][1]['outcomes']
+data = []
 
-def get_american_odds(outcome):
-    return int(outcome['price']['american'])
+def get_details(outcome):
+    return {
+        'name': outcome['description'],
+        'odds': int(outcome['price']['american']),
+    }
 
-odds = list(map(get_american_odds, outcomes))
+def get_outcomes(event):
+    outcomes = event['displayGroups'][0]['markets'][1]['outcomes']
+    return list(map(get_details, outcomes))
 
-print(odds)
+for tournament in odds_json:
+    events = tournament['events']
+    events_detailed = list(map(get_outcomes, events))
+    data = data + events_detailed
+
+print(data)
 
 # Get probability that player 1 will win
 url = 'https://www.ultimatetennisstatistics.com/h2hHypotheticalMatchup?playerId1=11127&playerId2=6269'
@@ -28,24 +38,25 @@ print(p1_win_prob)
 
 
 # Calculate who to bet on
-sim_p1_wins = p1_win_prob * 10
-sim_p2_wins = 1000 - sim_p1_wins
 
-p1_win = sim_p1_wins * (100/odds[0])
-p1_lose = sim_p2_wins * -1
-p2_lose = sim_p1_wins * -1
-p2_win = sim_p2_wins * (odds[1]/100)
+# sim_p1_wins = p1_win_prob * 10
+# sim_p2_wins = 1000 - sim_p1_wins
 
-p1_total = p1_win + p1_lose
-p2_total = p2_win + p2_lose
+# p1_win = sim_p1_wins * (100/odds[0])
+# p1_lose = sim_p2_wins * -1
+# p2_lose = sim_p1_wins * -1
+# p2_win = sim_p2_wins * (odds[1]/100)
 
-if p1_total < 0 and p2_total < 0:
-    print('Do not bet')
-else:
-    if p1_total > p2_total:
-        print('Bet on player 1')
-    else:
-        print('Bet on player 2')
+# p1_total = p1_win + p1_lose
+# p2_total = p2_win + p2_lose
 
-print(p1_total)
-print(p2_total)
+# if p1_total < 0 and p2_total < 0:
+#     print('Do not bet')
+# else:
+#     if p1_total > p2_total:
+#         print('Bet on player 1')
+#     else:
+#         print('Bet on player 2')
+
+# print(p1_total)
+# print(p2_total)
